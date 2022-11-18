ebd69f926d984ee7581b2d4de19ef16d9ed7bdb6
Arthur Korostelev <korostelev.art.ua@gmail.com>
2022-11-18 18:20:53 +0200

    created

---
+++ README.md
@@ -0,0 +1,7 @@
+goit-node-hw-CLI-appc
+CLI work with users contacts db
+
+Screenshots: https://ibb.co/NsmCqrt ,
+https://ibb.co/6sJ9Cdj ,
+https://ibb.co/tpbqVQT ,
+https://ibb.co/jyGfXgG
---
+++ contacts.js
@@ -0,0 +1,59 @@
+const fs = require("fs").promises;
+const path = require("node:path");
+const contactsPath = path.resolve("db/contacts.json");
+const { uid } = require("uid");
+
+function listContacts() {
+  getAllContacts().then(console.table).catch(console.log);
+}
+
+function getContactById(contactId) {
+  getAllContacts()
+    .then((data) => console.table(data.find(({ id }) => id === contactId)))
+    .catch(console.log);
+}
+
+function removeContact(contactId) {
+  getAllContacts()
+    .then((data) => {
+      const newContacts = data.filter(({ id }) => id !== contactId);
+      const stringifiedContacts = JSON.stringify(newContacts);
+      fs.writeFile(contactsPath, stringifiedContacts, errorFirstCallback);
+    })
+    .catch(console.log);
+}
+
+function addContact(name, email, phone) {
+  const id = uid();
+  const newContact = { id, name, email, phone };
+  getAllContacts()
+    .then((data) => {
+      const newContacts = [...data, newContact];
+      const stringifiedContacts = JSON.stringify(newContacts);
+      fs.writeFile(contactsPath, stringifiedContacts, errorFirstCallback);
+    })
+    .catch(console.log);
+}
+
+async function getAllContacts() {
+  try {
+    const data = await fs.readFile(contactsPath, { encoding: "utf8" });
+    return JSON.parse(data);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+function errorFirstCallback(err) {
+  if (err) {
+    throw err;
+  }
+}
+
+module.exports = {
+  contactsPath,
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
